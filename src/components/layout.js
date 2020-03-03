d14a6bbbf4030e70ab3a9576abb7d7b973387b67
Administrator <admin@example.com>
2020-03-03 22:00:40 +0100

    Add Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 import Container from "@material-ui/core/Container";
+import Footer from "./footer";
 
 import Header from "./header";
 import "./layout.css";
@@ -35,11 +36,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Footer />
       </div>
     </Container>
   );
