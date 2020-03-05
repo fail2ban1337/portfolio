166d84da3f7c90921261b2a1bacbbc3e65068d6d
Administrator <admin@example.com>
2020-03-05 17:44:25 +0100

    Remove unised components

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,9 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MailIcon from "@material-ui/icons/Mail";
-import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
-import blue from "@material-ui/core/colors/blue";
-import undrew from "../images/undraw_source_code_xx2e.svg";
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
 import { Link, animateScroll as scroll } from "react-scroll";
 
 const useStyles = makeStyles(theme => ({
@@ -196,7 +185,6 @@ export default function Header() {
               aria-controls={mobileMenuId}
               aria-haspopup="true"
               onClick={handleMobileMenuOpen}
-              color="black"
             >
               <MoreIcon />
             </IconButton>
